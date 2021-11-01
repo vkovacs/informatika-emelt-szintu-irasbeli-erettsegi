ed2839d31c4a45887e0c3bbe7cef57b354142f7e
Viktor Kovacs <crusader8@gmail.com>
2021-11-01 13:41:54 +0100

    read number on position

--- 2021/src/sudoku.py
+++ 2021/src/sudoku.py
@@ -7,15 +7,14 @@ input_file_path = "../resources/Forrasok/4_Sudoku/" + input_file_name
 
 input_file = open(input_file_path)
 
-sudo_matrix_row_size = 9
-sudo_matrix_col_size = 9
+sudo_matrix_max_row_size = 9
+sudo_matrix_max_col_size = 9
 
-sudoku_matrix = [[0] * sudo_matrix_col_size] * sudo_matrix_row_size # https://www.geeksforgeeks.org/python-using-2d-arrays-lists-the-right-way/
-
+sudoku_matrix = [[0] * sudo_matrix_max_col_size] * sudo_matrix_max_row_size # https://www.geeksforgeeks.org/python-using-2d-arrays-lists-the-right-way/
 
 line_counter = 0
 for line in input_file.readlines():
-    split_line = line.split()
+    split_line = list(map(int, line.split())) # we want a matrix which contains ints not strings: https://stackoverflow.com/a/6429930
 
     if len(split_line) == 9:
         sudoku_matrix[line_counter] = split_line
@@ -23,5 +22,16 @@ for line in input_file.readlines():
     else:
         break
 
-selected_row_num = input("Adja meg egy sor számát: ")
-selected_col_num = input("Adja meg egy oszlop számát: ")
+selected_row_num = int(input("Adja meg egy sor számát: "))
+selected_col_num = int(input("Adja meg egy oszlop számát: "))
+selected_row_index = selected_row_num - 1
+selected_col_index = selected_col_num - 1
+
+print("2. feladat")
+print("Az adott helyen szereplő szám: ")
+value_in_selected_position = sudoku_matrix[selected_row_index][selected_col_index]
+
+if value_in_selected_position > 0:
+    print(value_in_selected_position)
+else:
+    print("Az adott helyet még nem töltötték ki.")
