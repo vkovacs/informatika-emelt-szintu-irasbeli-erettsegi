890c826a7917f398832e56be94ff88ce54ab0106
Viktor Kovacs <viktor.kovacs@siemens.com>
2022-04-19 14:46:34 +0200

    solve3

--- 2014/src/nezoter.py
+++ 2014/src/nezoter.py
@@ -18,8 +18,20 @@ print(price_categories)
 
 # 2
 
-line = int(input("Sor: "))
-col = int(input("Sorszám: "))
+#line = int(input("Sor: "))
+#col = int(input("Sorszám: "))
+
+line = 1
+col = 1
 
 seat_status = "üres" if reservations[line - 1][col - 1] == "o" else "foglalt"
 print("A megadott szék " + seat_status)
+
+# 3
+sum_reserved = 0
+all_seats_count = 0
+for reservation_line in reservations:
+    sum_reserved = sum_reserved + reservation_line.count("o")
+    all_seats_count = all_seats_count + len(reservation_line)
+
+print(f"Az előadásra eddig {sum_reserved} jegyet adtak el, ez a nézőtér {round(sum_reserved / all_seats_count  * 100)} %-a.")
