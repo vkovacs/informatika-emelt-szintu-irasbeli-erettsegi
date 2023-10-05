abfdc4db8ebad666caae69d99a201aa6cef5e87e
Viktor Kovacs <viktor.kovacs@siemens.com>
2023-10-05 15:45:37 +0200

    add 6

--- 2012/szinkep.py
+++ 2012/szinkep.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 # 1
 input_file = open("kep.txt")
 
@@ -10,7 +12,7 @@ for line in input_file.readlines():
 
 colors_multi_dimensional_list = []
 
-for i in range(0, 2500, 49):
+for i in range(0, 2500, 50):
     colors_multi_dimensional_list.append(colors[i:i + 50])
 
 
@@ -52,15 +54,15 @@ blue = (0, 0, 255)
 
 redCount = 0
 for i in range(0, 50):
-   redCount = redCount + countColorInRow(red, i)
+    redCount = redCount + countColorInRow(red, i)
 
 greenCount = 0
 for i in range(0, 50):
-   greenCount = greenCount + countColorInRow(green, i)
+    greenCount = greenCount + countColorInRow(green, i)
 
 blueCount = 0
 for i in range(0, 50):
-   blueCount = blueCount + countColorInRow(blue, i)
+    blueCount = blueCount + countColorInRow(blue, i)
 
 maxColor = "Piros"
 if greenCount > redCount:
@@ -68,4 +70,35 @@ if greenCount > redCount:
 if (blueCount > greenCount):
     maxColor = "Kék"
 
-print(f"A piros színből {redCount} a zöld színből {greenCount} a kék színből {blueCount} darab van a képen. Tehát a legtöbb szín {maxColor}-ból van.")
+print(
+    f"A piros színből {redCount} a zöld színből {greenCount} a kék színből {blueCount} darab van a képen. Tehát a legtöbb szín {maxColor}-ból van.")
+
+# 5
+bordered_colors_multi_dimensional_list = colors_multi_dimensional_list[:]  # shallow copy
+
+black = (0, 0, 0)
+
+for i in range(0, 50):
+    # add horizontal borders
+    bordered_colors_multi_dimensional_list[0][i] = black
+    bordered_colors_multi_dimensional_list[1][i] = black
+    bordered_colors_multi_dimensional_list[2][i] = black
+
+    bordered_colors_multi_dimensional_list[47][i] = black
+    bordered_colors_multi_dimensional_list[48][i] = black
+    bordered_colors_multi_dimensional_list[49][i] = black
+
+    # add vertical borders
+    bordered_colors_multi_dimensional_list[i][0] = black
+    bordered_colors_multi_dimensional_list[i][1] = black
+    bordered_colors_multi_dimensional_list[i][2] = black
+
+    bordered_colors_multi_dimensional_list[i][47] = black
+    bordered_colors_multi_dimensional_list[i][48] = black
+    bordered_colors_multi_dimensional_list[i][49] = black
+
+flattened_bordered_colors_multi_dimensional_list = list(chain.from_iterable(
+    bordered_colors_multi_dimensional_list))  # flatten list: https://realpython.com/python-flatten-list/#chaining-iterables-with-itertoolschain
+
+for color in flattened_bordered_colors_multi_dimensional_list:
+    print(str(color[0]) + " " + str(color[1]) + " " + str(color[2])) # TODO: write to file..
