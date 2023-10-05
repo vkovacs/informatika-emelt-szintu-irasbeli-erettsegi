fc2d9e24dc5409a0273c7ee262e1ec9ddf0ab9c6
Viktor Kovacs <viktor.kovacs@siemens.com>
2023-10-05 14:40:19 +0200

    add 2

--- 2012/szinkep.py
+++ 2012/szinkep.py
@@ -1,14 +1,29 @@
+# 1
 input_file = open("kep.txt")
 
-pixels = []
+colors = []
 
 for line in input_file.readlines():
     split_line = line.strip().split(" ")
-    pixels.append(tuple(split_line))
+    integer_list = [int(s) for s in split_line] # the tuples should contain ints not strings
+    colors.append(tuple(integer_list))
 
-pixels_multi_dimensional_list = []
+colors_multi_dimensional_list = []
 
 for i in range(0, 2500, 49):
-    pixels_multi_dimensional_list.append(pixels[i:i + 50])
+    colors_multi_dimensional_list.append(colors[i:i + 50])
 
-print(pixels_multi_dimensional_list)
+
+# 2
+def isColorPresent(color):
+    for row in colors_multi_dimensional_list:
+        if color in row:
+            return True
+    return False
+
+user_pixel_present = (200, 96, 64)
+assert isColorPresent(user_pixel_present)
+user_pixel_not_present = (123, 123, 123)
+assert not isColorPresent(user_pixel_not_present)
+
+#3
