79753d1888f4bc21c3a248502fbb6cbb735507a4
Viktor Kovacs <viktor.kovacs@siemens.com>
2023-10-05 15:00:03 +0200

    add 3

--- 2012/szinkep.py
+++ 2012/szinkep.py
@@ -5,7 +5,7 @@ colors = []
 
 for line in input_file.readlines():
     split_line = line.strip().split(" ")
-    integer_list = [int(s) for s in split_line] # the tuples should contain ints not strings
+    integer_list = [int(s) for s in split_line]  # the tuples should contain ints not strings
     colors.append(tuple(integer_list))
 
 colors_multi_dimensional_list = []
@@ -21,9 +21,26 @@ def isColorPresent(color):
             return True
     return False
 
+
 user_pixel_present = (200, 96, 64)
 assert isColorPresent(user_pixel_present)
 user_pixel_not_present = (123, 123, 123)
 assert not isColorPresent(user_pixel_not_present)
 
-#3
+
+# 3
+def countColorInRow(color, rowIndex):
+    return colors_multi_dimensional_list[rowIndex].count(color)
+
+
+def countColorInColumn(color, colIndex):
+    colorCounter = 0
+    for i in range(0, 50):
+        if (colors_multi_dimensional_list[i][7] == color):
+            colorCounter = colorCounter + 1
+
+    return colorCounter
+
+colorToCount = colors_multi_dimensional_list[34][7]
+print(f"Sorban: {countColorInRow(colorToCount, 34)} Oszlopban: {countColorInColumn(colorToCount, 7)}")
+
