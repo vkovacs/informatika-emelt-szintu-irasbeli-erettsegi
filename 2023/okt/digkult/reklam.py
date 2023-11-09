1ea1c2a5cb6b04349537070fa29bb9f00dd1d1db
Viktor Kovacs <viktor.kovacs@siemens.com>
2023-11-09 13:38:59 +0100

    solve 3

--- 2023/okt/digkult/reklam.py
+++ 2023/okt/digkult/reklam.py
@@ -5,7 +5,7 @@ orders = []
 
 for line in orders_file.readlines():
     split_line = line.strip().split(" ")
-    order_item = [int(split_line[0]), split_line[1], int(split_line[2])]
+    order_item = (int(split_line[0]), split_line[1], int(split_line[2]))
 
     orders.append(order_item)
 
@@ -14,3 +14,14 @@ print(orders)
 # 2. feladat
 
 print(f"Az össes rendelés darabszáma: {len(orders)}")
+
+# 3. feladat
+
+day = 9  # FIXME: read from input
+
+order_count_for_a_day = 0
+for orders in orders:
+    if orders[0] == day:
+        order_count_for_a_day += 1
+
+print(f"A rendelések száma az adott napon: {order_count_for_a_day}")
