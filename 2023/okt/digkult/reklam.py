33930504d0bd46ac1ecc92b4995ca50f34e86b40
Viktor Kovacs <viktor.kovacs@siemens.com>
2023-11-09 14:02:22 +0100

    solve 4

--- 2023/okt/digkult/reklam.py
+++ 2023/okt/digkult/reklam.py
@@ -20,8 +20,20 @@ print(f"Az össes rendelés darabszáma: {len(orders)}")
 day = 9  # FIXME: read from input
 
 order_count_for_a_day = 0
-for orders in orders:
-    if orders[0] == day:
+for order in orders:
+    if order[0] == day:
         order_count_for_a_day += 1
 
 print(f"A rendelések száma az adott napon: {order_count_for_a_day}")
+
+# 4. feladat
+
+days_with_order_in_NR = set()
+for order in orders:
+    if order[1] == "NR":
+        days_with_order_in_NR.add(order[0])
+
+print(days_with_order_in_NR)
+
+days_count_without_order_in_NR = len(set(range(1, 31)) - days_with_order_in_NR)
+print(f"{days_count_without_order_in_NR} nap nem volt a reklámban nem érintett városból rendelés")
