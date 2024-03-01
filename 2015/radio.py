92009ad5ac0a7383fc2c460d46f7d9dae5fbfcd7
Viktor Kovacs <viktor.kovacs@siemens.com>
2024-03-01 15:03:09 +0100

    solution 5

--- 2015/radio.py
+++ 2015/radio.py
@@ -36,3 +36,30 @@ for metadata in signals_metadata:
 for day_index in range(len(signal_count_per_day)):
     print(f'On the {day_index + 1} day there were {signal_count_per_day[day_index]} message')
 
+# 5
+all_signal_for_a_day = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: []}
+for i in range(len(signals_metadata)):
+    day = signals_metadata[i][0]
+    all_signal_for_a_day[day].append(signals[i])
+
+
+# print(all_signal_for_a_day)
+
+def merged_character(lists, index):
+    for list in lists:
+        if list[index] != '#':
+            return list[index]
+    return '#'
+
+
+merged_signals_for_a_day = dict()
+
+for day in all_signal_for_a_day:  # iterate over the dict day is the key
+    signals_for_a_day = all_signal_for_a_day[day]
+    merged_signal = ''
+    for signal_index in range(len(signals_for_a_day[0])): # for every character index of a message (all messages has the same legnth for a given day) merge the characters of all the messages prefer not # over #
+        merged_signal = merged_signal + merged_character(all_signal_for_a_day[day], signal_index)
+    merged_signals_for_a_day[day] = merged_signal
+
+print(merged_signals_for_a_day)
+
