f94686dc138ffd3e62f34fef27f296921c39f655
Viktor Kovacs <viktor.kovacs@siemens.com>
2024-03-01 15:35:51 +0100

    solution 7

--- 2015/radio.py
+++ 2015/radio.py
@@ -57,9 +57,54 @@ merged_signals_for_a_day = dict()
 for day in all_signal_for_a_day:  # iterate over the dict day is the key
     signals_for_a_day = all_signal_for_a_day[day]
     merged_signal = ''
-    for signal_index in range(len(signals_for_a_day[0])): # for every character index of a message (all messages has the same legnth for a given day) merge the characters of all the messages prefer not # over #
+    for signal_index in range(len(signals_for_a_day[0])):  # for every character index of a message (all messages has the same legnth for a given day) merge the characters of all the messages prefer not # over #
         merged_signal = merged_signal + merged_character(all_signal_for_a_day[day], signal_index)
     merged_signals_for_a_day[day] = merged_signal
 
 print(merged_signals_for_a_day)
 
+
+# 6
+def szame(szo):
+    for i in szo:
+        if i < 0 or i > 9:
+            return False
+    return True
+
+
+# 7
+def radio_amateur_signal_for_a_day(day, radio_amateur_id):
+    for i in range(len(signals_metadata)):
+        if signals_metadata[i][0] == day and signals_metadata[i][1] == radio_amateur_id:
+            return signals[i]
+
+
+# -1: no entity count description in signal. -2: invalid entity count description. otherwise the number of entities
+def determine_entity_count(signal):
+    if '/' not in signal:
+        return -1
+
+    entity_description = signal.split(" ")[0]
+
+    if '#' in entity_description:
+        return -2
+
+    entities_count_string = entity_description.split('/')
+    return int(entities_count_string[0]) + int (entities_count_string[1])
+
+def determine_entity_count_message(signal):
+    count = determine_entity_count(signal)
+
+    if (count == -1):
+        return "Nincs ilyen feljegyzés"
+    if (count == -2):
+        return "Nincs információ"
+
+    return count
+
+day = 2
+radio_amateur_id = 15
+
+print(determine_entity_count_message(radio_amateur_signal_for_a_day(2, 15)))
+print(determine_entity_count_message(radio_amateur_signal_for_a_day(6, 19)))
+print(determine_entity_count_message(radio_amateur_signal_for_a_day(10, 7)))
